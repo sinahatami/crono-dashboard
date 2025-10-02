498fd462bac48a5842623b9a4e623a6449b304ea
Sina <sinaa13781999@gmail.com>
2025-10-02 14:14:49 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-slate-900 min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-cyan-400">
+        Crono Dashboard Project: Setup Complete!
+      </h1>
+    </div>
+  )
+}
+
+export default App
